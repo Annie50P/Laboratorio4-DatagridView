8b54f908da5775a0bf977e34fa0a4a38d24998b4
Annie50P <aparicioann477@gmail.com>
2024-09-21 21:02:14 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Laboratorio4_DatagridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.dgvReservas = new System.Windows.Forms.DataGridView();
+            this.btagregar = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btAgregarReserva = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Tan;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(690, 31);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Laboratorio4_DatagridView.Properties.Resources.icone_x_grise_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(651, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 23);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lbTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lbTitulo.Location = new System.Drawing.Point(251, 48);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(212, 25);
+            this.lbTitulo.TabIndex = 6;
+            this.lbTitulo.Text = "GESTIÓN DE RESERVAS";
+            // 
+            // dgvReservas
+            // 
+            this.dgvReservas.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvReservas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvReservas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReservas.Location = new System.Drawing.Point(38, 290);
+            this.dgvReservas.Name = "dgvReservas";
+            this.dgvReservas.Size = new System.Drawing.Size(610, 168);
+            this.dgvReservas.TabIndex = 7;
+            // 
+            // btagregar
+            // 
+            this.btagregar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btagregar.Location = new System.Drawing.Point(305, 89);
+            this.btagregar.Name = "btagregar";
+            this.btagregar.Size = new System.Drawing.Size(96, 34);
+            this.btagregar.TabIndex = 8;
+            this.btagregar.Text = "Iniciar";
+            this.btagregar.UseVisualStyleBackColor = true;
+            this.btagregar.Click += new System.EventHandler(this.btagregar_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Laboratorio4_DatagridView.Properties.Resources.suite;
+            this.pictureBox4.Location = new System.Drawing.Point(485, 136);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(163, 128);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Laboratorio4_DatagridView.Properties.Resources.doble;
+            this.pictureBox3.Location = new System.Drawing.Point(274, 136);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(163, 128);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Laboratorio4_DatagridView.Properties.Resources.individual;
+            this.pictureBox2.Location = new System.Drawing.Point(38, 136);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(177, 128);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btAgregarReserva
+            // 
+            this.btAgregarReserva.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAgregarReserva.Location = new System.Drawing.Point(522, 486);
+            this.btAgregarReserva.Name = "btAgregarReserva";
+            this.btAgregarReserva.Size = new System.Drawing.Size(156, 34);
+            this.btAgregarReserva.TabIndex = 12;
+            this.btAgregarReserva.Text = "Agregar Reserva";
+            this.btAgregarReserva.UseVisualStyleBackColor = true;
+            this.btAgregarReserva.Click += new System.EventHandler(this.btAgregarReserva_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(690, 532);
+            this.Controls.Add(this.btAgregarReserva);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btagregar);
+            this.Controls.Add(this.dgvReservas);
+            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.DataGridView dgvReservas;
+        private System.Windows.Forms.Button btagregar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button btAgregarReserva;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio4_DatagridView
+{
+
+
+    public partial class Form1 : Form
+    {
+        DateTimePicker eleccionTiempo = new DateTimePicker();
+        Rectangle cellRectangle;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dgvReservas.AllowUserToAddRows = false;
+            dgvReservas.Controls.Add(eleccionTiempo);
+            eleccionTiempo.Visible = false;
+            eleccionTiempo.Format = DateTimePickerFormat.Short;
+            eleccionTiempo.MinDate = DateTime.Today; 
+            eleccionTiempo.TextChanged += new EventHandler(eleccionTiempo_TextChange);
+            dgvReservas.CellClick += new DataGridViewCellEventHandler(dgvReservas_CellClick);
+            dgvReservas.CellEndEdit += new DataGridViewCellEventHandler(dgvReservas_CellEndEdit);
+        }
+
+        private void eleccionTiempo_TextChange(Object sender, EventArgs e)
+        {
+            if (dgvReservas.CurrentCell != null)
+            {
+                
+                dgvReservas.CurrentCell.Value = eleccionTiempo.Value.ToString("dd/MM/yyyy");
+
+                if (dgvReservas.CurrentCell.OwningColumn.Name == "FechaSalida")
+                {
+                    DateTime fechaEntrada;
+                    if (DateTime.TryParse(dgvReservas.Rows[dgvReservas.CurrentCell.RowIndex].Cells["FechaEntrada"].Value?.ToString(), out fechaEntrada))
+                    {
+                       
+                        if (eleccionTiempo.Value <= fechaEntrada)
+                        {
+                            MessageBox.Show("La fecha de salida no puede ser anterior a la fecha de entrada.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            dgvReservas.CurrentCell.Value = null; 
+                        }
+                        else
+                        {
+                            // Calcular días de estadía
+                            int diasEstadia = (eleccionTiempo.Value - fechaEntrada).Days;
+                            dgvReservas.Rows[dgvReservas.CurrentCell.RowIndex].Cells["DiasEstadia"].Value = diasEstadia;
+
+                            // Calcular el costo total
+                            CalcularCostoTotal(dgvReservas.CurrentCell.RowIndex);
+                        }
+                    }
+                }
+                else if (dgvReservas.CurrentCell.OwningColumn.Name == "FechaEntrada")
+                {
+                    dgvReservas.Rows[dgvReservas.CurrentCell.RowIndex].Cells["DiasEstadia"].Value = null;
+                    dgvReservas.Rows[dgvReservas.CurrentCell.RowIndex].Cells["CostoTotal"].Value = null;
+                }
+            }
+        }
+
+        private void CalcularCostoTotal(int rowIndex)
+        {
+            var tipoHabitacion = dgvReservas.Rows[rowIndex].Cells["TipoHabitacion"].Value?.ToString();
+            int diasEstadia = Convert.ToInt32(dgvReservas.Rows[rowIndex].Cells["DiasEstadia"].Value ?? 0);
+            double tarifaPorNoche = 0;
+            double costoTotal = 0;
+
+
+            if (tipoHabitacion == "Individual")
+                tarifaPorNoche = 50;
+            else if (tipoHabitacion == "Doble")
+                tarifaPorNoche = 75;
+            else if (tipoHabitacion == "Suite")
+                tarifaPorNoche = 120;
+
+            // Calcular costo 
+            costoTotal = tarifaPorNoche * diasEstadia;
+
+            // Calcular ITBMS
+            double CostoConItbms = costoTotal * 0.07;
+            costoTotal =costoTotal+ CostoConItbms; 
+
+            
+            dgvReservas.Rows[rowIndex].Cells["CostoTotal"].Value = costoTotal;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btagregar_Click(object sender, EventArgs e)
+        {
+            string[] nombresColumnas = { "Nombre Huésped", "Fecha de entrada", "Fecha de salida", "Tipo de habitación", "Días de estadía", "Costo total" };
+            int[] anchosColumnas = { 150, 80, 80, 80, 80, 80 };
+
+            
+            if (dgvReservas.Columns.Count == 0)
+            {
+                //columna "Nombre Huésped"
+                DataGridViewTextBoxColumn columnaNombre = new DataGridViewTextBoxColumn
+                {
+                    HeaderText = "Nombre",
+                    Name = "Nombre",
+                    Width = anchosColumnas[0]
+                };
+                dgvReservas.Columns.Add(columnaNombre);
+
+                // columna "Fecha de entrada"
+                DataGridViewTextBoxColumn columnaFechaEntrada = new DataGridViewTextBoxColumn
+                {
+                    HeaderText = "Fecha de entrada",
+                    Name = "FechaEntrada",
+                    Width = anchosColumnas[1]
+                };
+                dgvReservas.Columns.Add(columnaFechaEntrada);
+
+                //columna "Fecha de salida"
+                DataGridViewTextBoxColumn columnaFechaSalida = new DataGridViewTextBoxColumn
+                {
+                    HeaderText = "Fecha de salida",
+                    Name = "FechaSalida",
+                    Width = anchosColumnas[2]
+                };
+                dgvReservas.Columns.Add(columnaFechaSalida);
+
+                //columna "Tipo de habitación"
+                DataGridViewComboBoxColumn columnaTipoHabitacion = new DataGridViewComboBoxColumn
+                {
+                    HeaderText = "Tipo de habitación",
+                    Name = "TipoHabitacion",
+                    Width = anchosColumnas[3],
+                    Items = { "Individual", "Doble", "Suite" }
+                };
+                dgvReservas.Columns.Add(columnaTipoHabitacion);
+
+                // columnas "Días de estadía" y "Costo total"
+                DataGridViewTextBoxColumn columnaDiasEstadia = new DataGridViewTextBoxColumn
+                {
+                    HeaderText = "Días de estadía",
+                    Name = "DiasEstadia",
+                    Width = anchosColumnas[4]
+                };
+                dgvReservas.Columns.Add(columnaDiasEstadia);
+
+                DataGridViewTextBoxColumn columnaCostoTotal = new DataGridViewTextBoxColumn
+                {
+                    HeaderText = "Costo total",
+                    Name = "CostoTotal",
+                    Width = anchosColumnas[5]
+                };
+                dgvReservas.Columns.Add(columnaCostoTotal);
+            }
+
+            // Deshabilitar el botón al presionarlo jejje
+            btagregar.Enabled = false;
+        }
+
+        private void dgvReservas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+            if (e.ColumnIndex == dgvReservas.Columns["FechaEntrada"].Index || e.ColumnIndex == dgvReservas.Columns["FechaSalida"].Index)
+            {
+                
+                cellRectangle = dgvReservas.GetCellDisplayRectangle(e.ColumnIndex, e.RowIndex, true);
+                eleccionTiempo.Size = new Size(cellRectangle.Width, cellRectangle.Height);
+                eleccionTiempo.Location = new Point(cellRectangle.X, cellRectangle.Y);
+
+                if (dgvReservas.CurrentCell.Value != null)
+                {
+                    DateTime tempDate;
+                    if (DateTime.TryParse(dgvReservas.CurrentCell.Value.ToString(), out tempDate))
+                    {
+                        eleccionTiempo.Value = tempDate;
+                    }
+                    else
+                    {
+                        eleccionTiempo.Value = DateTime.Now; 
+                    }
+                }
+
+                
+                eleccionTiempo.Visible = true;
+            }
+            else
+            {
+                eleccionTiempo.Visible = false;
+            }
+        }
+
+        private void dgvReservas_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            eleccionTiempo.Visible = false;
+        }
+
+        private void btAgregarReserva_Click(object sender, EventArgs e)
+        {
+            dgvReservas.Rows.Add();
+        }
+    }
+}
